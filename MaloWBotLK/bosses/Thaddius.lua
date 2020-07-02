71a52f526389f8f2d3e92c169942100956d7583c
MaloW <lowegrenmarcus@gmail.com>
2020-07-02 09:54:02 +0200

    Thaddius improvements, automatic slowfall/levitate.
    Signed-off-by: MaloW <lowegrenmarcus@gmail.com>

--- MaloWBotLK/bosses/Thaddius.lua
+++ MaloWBotLK/bosses/Thaddius.lua
@@ -9,6 +9,35 @@ function mb_BossModule_Thaddius_PreOnUpdate()
         return mb_GoToPosition_Update(0.25859490, 0.15420512, 0.003)
     end
     mb_GoToPosition_Reset()
+
+    local slowFallSpell = nil
+    if mb_GetClass("player") == "MAGE" then
+        slowFallSpell = "Slow Fall"
+    elseif mb_GetClass("player") == "PRIEST" then
+        slowFallSpell = "Levitate"
+    end
+    if slowFallSpell ~= nil then
+        if mb_commanderUnit == nil then
+            return false
+        end
+        local unit = mb_commanderUnit .. "target"
+        if UnitExists(unit) and UnitName(unit) ~= "Thaddius" and mb_UnitHealthPercentage(unit) < 30 then
+            return mb_BossModule_Thaddius_BlanketRaidSlowFall(slowFallSpell)
+        end
+    end
+    return false
+end
+
+function mb_BossModule_Thaddius_BlanketRaidSlowFall(spell)
+    local members = mb_GetNumPartyOrRaidMembers()
+    for i = 1, members do
+        local unit = mb_GetUnitFromPartyOrRaidIndex(i)
+        if mb_GetBuffTimeRemaining(unit, "Slow Fall") < 3 and mb_GetBuffTimeRemaining(unit, "Levitate") < 3 then
+            if mb_CastSpellOnFriendly(unit, spell) then
+                return true
+            end
+        end
+    end
     return false
 end
 
